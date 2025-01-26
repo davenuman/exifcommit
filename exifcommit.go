67334b3dede4fd35bf2f426c0a728858a1e70a47
David Numan <davidnuman@gmail.com>
2025-01-26 17:38:03 -0500

    Attemping EXIF writes with go-exif.
    However, not able to get this to work. It fails on line 109 with ConstructExifBuilder.

--- exifcommit.go
+++ exifcommit.go
@@ -2,18 +2,134 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
+
+	exif "github.com/dsoprea/go-exif/v3"
+	exifcommon "github.com/dsoprea/go-exif/v3/common"
+	jis "github.com/dsoprea/go-jpeg-image-structure/v2"
 )
+
+const exifLabel = "ImageDescription"
 
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Read the current value of the tag in the targetFile.
+func readTag(targetFile string) string {
+	// fmt.Printf("attempting EXIF on file <%s> tag [%s]\n", targetFile, tagValue)
+
+	// jmp := NewJpegMediaParser()
+	rawExif, err := exif.SearchFileAndExtractExif(targetFile)
+	checkErr(err)
+
+	im, err := exifcommon.NewIfdMappingWithStandard()
+	checkErr(err)
+
+	ti := exif.NewTagIndex()
+
+	_, index, err := exif.Collect(im, ti, rawExif)
+	checkErr(err)
+
+	tagName := exifLabel
+
+	rootIfd := index.RootIfd
+
+	// We know the tag we want is on IFD0 (the first/root IFD).
+	results, err := rootIfd.FindTagWithName(tagName)
+	checkErr(err)
+
+	// This should never happen.
+	if len(results) != 1 {
+		log.Panicf("there wasn't exactly one result")
+	}
+	ite := results[0]
+
+	valueRaw, err := ite.Value()
+	checkErr(err)
+
+	value := valueRaw.(string)
+	fmt.Println(value)
+	return value
+}
+
+// We do this not because it is easy...
+func writeTag(targetFile string, newDescription string) bool {
+	rawExif, err := exif.SearchFileAndExtractExif(targetFile)
+	checkErr(err)
+
+	im, err := exifcommon.NewIfdMappingWithStandard()
+	checkErr(err)
+
+	ti := exif.NewTagIndex()
+
+	_, index, err := exif.Collect(im, ti, rawExif)
+	checkErr(err)
+
+	rootIfd := index.RootIfd
+
+	// We know the tag we want is on IFD0 (the first/root IFD).
+	results, err := rootIfd.FindTagWithName(exifLabel)
+	checkErr(err)
+
+	// This should never happen.
+	if len(results) != 1 {
+		log.Panicf("there wasn't exactly one result")
+	}
+	ite := results[0]
+
+	valueRaw, err := ite.Value()
+	checkErr(err)
+
+	value := valueRaw.(string)
+	fmt.Println(value)
+
+	// jpegstructure method
+
+	fmt.Printf("attempting EXIF on file <%s> tag [%s]\n", targetFile, newDescription)
+
+	jmp := jis.NewJpegMediaParser()
+
+	intfc, err := jmp.ParseFile(targetFile)
+	checkErr(err)
+
+	sl := intfc.(*jis.SegmentList)
+	// sl.Print()
+
+	// Update the tag.
+
+	rootIb, err := sl.ConstructExifBuilder()
+	checkErr(err)
+	fmt.Println("does not get this far.")
+
+	ifdPath := "IFD0"
+
+	ifdIb, err := exif.GetOrCreateIbFromRootIb(rootIb, ifdPath)
+	checkErr(err)
+
+	fmt.Println(ifdIb)
+	err = ifdIb.SetStandardWithName(exifLabel, newDescription)
+	checkErr(err)
+
+	// Update the exif segment.
+
+	err = sl.SetExif(rootIb)
+	checkErr(err)
+
+	b := new(bytes.Buffer)
+
+	err = sl.Write(b)
+	checkErr(err)
+
+	return true
 }
 
 func parseCommitFile(fileName string) {
@@ -30,7 +146,7 @@ func parseCommitFile(fileName string) {
 
 	var splits []string
 	// Scan for the target files.
-	r := regexp.MustCompile(`# file: `)
+	r := regexp.MustCompile(`file: `)
 	for fileScan.Scan() {
 		splits = r.Split(fileScan.Text(), 2)
 		if len(splits) > 1 {
@@ -38,14 +154,14 @@ func parseCommitFile(fileName string) {
 		}
 	}
 
-	fmt.Println(targetFiles)
+	for _, tf := range targetFiles {
+		writeTag(tf, newDescription)
+	}
 
 	// TODO: Process EXIF change
 }
 
 func main() {
-	exifLabel := "ImageDescription"
-
 	// Handle arg or input.
 	var searchTerm string
 	if len(os.Args) > 2 {
@@ -84,9 +200,11 @@ func main() {
 	commitMessage += "\n# (remove to exclude from editing):\n#"
 
 	// Add list of files
+	var description string
 	for _, fn := range fileList {
 		// TODO: Read current EXIF tag
-		commitMessage += fmt.Sprintf("\n# file: %s\n%s", fn, "Place holder description\n")
+		description = readTag(fn)
+		commitMessage += fmt.Sprintf("\n# file: %s\n%s", fn, description)
 	}
 	tmpFile.WriteString(commitMessage)
 	tmpFile.Sync()
@@ -97,7 +215,6 @@ func main() {
 		// Fall back to a logical default
 		editor = "vim"
 	}
-	fmt.Printf("Using editor: %s\n", editor)
 	cmd := exec.Command(editor, tmpFile.Name())
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
